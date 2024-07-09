61f33f7bbb27efcbca89ec74b23cf5971507d6ee
Braian A. Diez <bdiez19@gmail.com>
2024-07-09 19:35:57 -0300

    chore: Update Nuxt configuration and package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-04-03",
-	devtools: { enabled: true },
-	runtimeConfig: {
+    compatibilityDate: "2024-04-03",
+    devtools: { enabled: true },
+
+    runtimeConfig: {
 		openaiApiKey: process.env.OPENAI_API_KEY,
 	},
+
+    modules: ["@unocss/nuxt"]
 });
