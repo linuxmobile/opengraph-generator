7e17ba5afa196fe2ed3565bf7c79a35d88185989
Braian A. Diez <bdiez19@gmail.com>
2024-07-11 19:17:33 -0300

    chore: Update PlaceurlInput component styling and form submission handling

--- components/placeurl-input.vue
+++ components/placeurl-input.vue
@@ -1,7 +1,7 @@
 <template>
   <form @submit.prevent="handleSubmit" class="flex items-center justify-center">
     <div class="relative">
-      <input type="text" v-model="url" class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none" placeholder="https://linu.dev/"/>
+      <input type="text" v-model="url" class="px-4 py-2 border border-gray-800 text-white bg-neutral-900 rounded-md focus:outline-none" placeholder="https://linu.dev/"/>
       <button type="submit" class="absolute top-1/2 right-3 -translate-y-1/2 text-gray-400 bg-transparent border-none p-0">
         <Search />
       </button>
@@ -14,14 +14,15 @@ import { Search } from 'lucide-vue-next';
 
 const url = ref('');
 
+const { setMetadata } = useMetadata();
+
 const handleSubmit = async () => {
   try {
     const fetchedMetadata = await $fetch(`/api/extract-metadata?url=${encodeURIComponent(url.value)}`);
-    useMetadata(fetchedMetadata);
-    console.log('Metadata fetched:', fetchedMetadata);
+    setMetadata(fetchedMetadata);
   } catch (error) {
     console.error('Error fetching metadata:', error);
-    useMetadata(null);
+    setMetadata(null);
   }
 };
 </script>
