e1ffe39c42aa7f1ed64a442f094c2ddb31daf6b2
Braian A. Diez <bdiez19@gmail.com>
2024-07-15 14:48:34 -0300

    chore: Refactor useMetadata to handle optional keywords and author

--- composables/useMetadata.ts
+++ composables/useMetadata.ts
@@ -4,22 +4,29 @@ interface Metadata {
   url: string;
   title: string | null;
   description: string | null;
-  keywords: string[] | null;
-  author: string | null;
+  keywords?: string[];
+  author?: string;
 }
 
 export const useMetadata = () => {
   const [metadata, setMetadata] = useGlobalGenericState<Metadata>('metadata', {} as Metadata);
 
   const setFilteredMetadata = (newMetadata: Metadata) => {
-    const filteredMetadata: Metadata = {
+    const filteredMetadata: Partial<Metadata> = {
       url: newMetadata.url,
       title: newMetadata.title,
       description: newMetadata.description,
-      keywords: newMetadata.keywords,
-      author: newMetadata.author,
     };
-    setMetadata(filteredMetadata);
+
+    if (newMetadata.keywords && newMetadata.keywords.length > 0) {
+      filteredMetadata.keywords = newMetadata.keywords;
+    }
+
+    if (newMetadata.author) {
+      filteredMetadata.author = newMetadata.author;
+    }
+
+    setMetadata(filteredMetadata as Metadata);
   };
 
   return {
