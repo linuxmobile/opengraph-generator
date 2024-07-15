4a4d3554264f445fb368a5641c8c033a597a4f43
Braian A. Diez <bdiez19@gmail.com>
2024-07-15 14:48:47 -0300

    feat: Add SliderPreviewOg component for displaying gradients in OpenGraph page

---
+++ components/SliderPreviewOg.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="bg-neutral-900 w-full h-60% py-4">
+    <div class="h-full flex flex-row gap-x-2 max-w-screen overflow-x-auto px-4">
+      <div v-for="gradient in orderedGradients" :key="gradient.id" class="rounded-xl aspect-video w-auto h-50% p-3" :class="classes" :style="{ background: gradient.gradient }">
+        <p>{{ title }}</p>
+        <p>{{ description }}</p>
+        <p>{{ author }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+
+const gradients = ref([])
+const { fewRandomGradients, setOrderedGradients, orderedGradients } = useGradients();
+
+defineProps({
+  title: String,
+  description: String,
+  author: String,
+  classes: String,
+})
+
+onMounted(() => {
+  setOrderedGradients(fewRandomGradients);
+  gradients.value = orderedGradients.value
+})
+</script>
--- pages/opengraph/index.vue
+++ pages/opengraph/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <main class="">
-    <div v-for="(value, key) in metadata" :key="key" class="metadata-item">
-      <strong>{{ key }}:</strong> {{ value }}
-    </div>
+  <main class="relative h-screen w-full flex flex-col-reverse">
+    <SliderPreviewOg
+    :title="metadata.title"
+    :description="metadata.description"
+    :author="metadata.author"
+    />
+    <section class="h-full">
+      <div v-for="(value, key) in metadata" :key="key">
+        <strong>{{ key }}:</strong> {{ value }}
+      </div>
+    </section>
   </main>
 </template>
 <script setup>
+import SliderPreviewOg from '~/components/SliderPreviewOg.vue';
+
 const { metadata } = useMetadata();
 </script>
