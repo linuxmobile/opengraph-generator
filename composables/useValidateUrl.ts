652e51d0f95790e2e12665691293bfb128208955
Braian A. Diez <bdiez19@gmail.com>
2024-08-07 23:48:59 -0300

    refactor: Improve URL validation and metadata retrieval in useValidateUrl.ts

--- composables/useValidateUrl.ts
+++ composables/useValidateUrl.ts
@@ -1,7 +1,7 @@
-export function useValidateUrl(url: string): boolean {
+export async function useValidateUrl(url: string): Promise<boolean> {
 	try {
 		const urlPattern =
-			/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+			/^(https?:\/\/)?(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/gi;
 		if (!urlPattern.test(url)) {
 			alert("La URL no es válida");
 			return false;
@@ -12,7 +12,23 @@ export function useValidateUrl(url: string): boolean {
 			url = "http://" + url;
 		}
 
-		return true;
+		try {
+			const response = await fetch(
+				`/api/extract-metadata?url=${encodeURIComponent(url)}`,
+			);
+			const metadata = await response.json();
+
+			if (metadata.error) {
+				alert("La URL no es accesible o no es una página web válida");
+				return false;
+			}
+
+			return true;
+		} catch (error) {
+			console.error("Error al validar la URL:", error);
+			alert("Error al validar la URL");
+			return false;
+		}
 	} catch (error) {
 		console.error("Error al validar la URL:", error);
 		alert("Error al validar la URL");
