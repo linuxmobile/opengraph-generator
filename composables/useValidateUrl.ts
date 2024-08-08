61381bc527e57a2691043d880e6ce6befd1f47da
Braian A. Diez <bdiez19@gmail.com>
2024-08-08 13:58:31 -0300

    refactor: Improve URL validation and error handling in useValidateUrl.ts

--- composables/useValidateUrl.ts
+++ composables/useValidateUrl.ts
@@ -3,20 +3,18 @@ export async function useValidateUrl(url: string): Promise<boolean> {
 		const urlPattern =
 			/^(https?:\/\/)?(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}(\/[a-zA-Z0-9@:%_\+.~#?&//=]*)?$/gi;
 		if (!urlPattern.test(url)) {
-			alert("La URL no es válida");
+			alert("The URL is not valid");
 			return false;
 		}
 
-		// Añade el protocolo si falta
 		if (!/^https?:\/\//i.test(url)) {
 			url = "http://" + url;
 		}
 
-		// Verificar si la URL contiene un archivo específico
 		const filePattern =
 			/\/[^\/]+\.(md|txt|html|htm|pdf|doc|docx|xls|xlsx|ppt|pptx|json|xml|csv)$/i;
 		if (filePattern.test(url)) {
-			alert("La URL no debe contener archivos específicos");
+			alert("The URL must not contain specific files");
 			return false;
 		}
 
@@ -27,18 +25,18 @@ export async function useValidateUrl(url: string): Promise<boolean> {
 			const metadata = await response.json();
 
 			if (metadata.error) {
-				alert("La URL no es accesible o no es una página web válida");
+				alert("The URL is not accessible or is not a valid web page");
 				return false;
 			}
 
 			return true;
 		} catch (error) {
-			console.error("Error al validar la URL:", error);
+			console.error("Error validating URL:", error);
 			alert("Error al validar la URL");
 			return false;
 		}
 	} catch (error) {
-		console.error("Error al validar la URL:", error);
+		console.error("Error validating URL:", error);
 		alert("Error al validar la URL");
 		return false;
 	}
